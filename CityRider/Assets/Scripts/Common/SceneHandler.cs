ab66ea9362eb8ec632baad5671dfeb5dfd3e5677
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2020-10-26 18:14:49 +0100

    #3 -> Main City Rider scene + SceneHandler script

---
+++ CityRider/Assets/Scripts/Common/SceneHandler.cs
@@ -0,0 +1,39 @@
+﻿/*
+ TO:DO =>
+            -> Smooth transition via animator
+                    -> Fade In / Fade Out
+ 
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Common;
+
+public class SceneHandler : MonoBehaviour
+{
+   
+    public void SwitchToMainScene()
+    {
+        if (!SceneExists(Consts.Scene_Main)) {
+            Debug.LogError("SceneHandler --> Scene named: " + Consts.Scene_Main + " is not an active scene. Please add it to open scenes in build settings :)");
+            return;
+        }
+        SceneManager.LoadScene(Consts.Scene_Main);
+    }
+
+    public void SwitchToMenuScene()
+    {
+        if (!SceneExists(Consts.Scene_Menu))
+        {
+            Debug.LogError("SceneHandler --> Scene named: " + Consts.Scene_Menu + " is not an active scene. Please add it to open scenes in build settings :)");
+            return;
+        }
+        SceneManager.LoadScene(Consts.Scene_Menu);
+    }
+
+    private bool SceneExists(string scene_name)
+    {
+        return SceneManager.GetSceneByName(scene_name) != null;
+    }
+}
